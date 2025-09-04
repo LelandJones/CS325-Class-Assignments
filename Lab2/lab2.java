ceabc974a61365dbae490a5dac4102734ffe884d
LelandJones <lelandj7@outlook.com>
2025-09-04 19:33:51 -0400

    added code

--- Lab2/lab2.java
+++ Lab2/lab2.java
@@ -10,28 +10,54 @@ public class lab2 {
 
 	public static void main(String[] args) 
 	{
-		int num = 0;
+		int N = 1000;
 		
 		//add code to get num from command line argument
 		if (args.length > 0)
         {
-            int N = 0;
             N = Integer.parseInt( args[0] );
             System.out.println( "N = " + N);
         }
         else
         {
-            System.out.println("Missing argument");
+            System.out.println("No user input. N= 1000");
         }
         
 		//call showPrimes
-		showPrimes( num );
+		showPrimes( N );
 		
 	}//main
 	
 	public static void showPrimes(int N)
 	{
-		//add code to display primes		
+		boolean[] isPrime = new boolean[N+1];
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for (int i = 2; i <= N; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++)
+        {
+            if (isPrime[i]) 
+            {
+                for (int j = i * i; j <= N; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= N; i++)
+        {
+            if (isPrime[i])
+            {
+                System.out.print(i + " ");
+            }
+        }
+
 	}//showPrimes
 	
 }//lab2
