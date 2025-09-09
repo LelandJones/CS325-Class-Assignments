0895754af76c404419fd53ecf03a97a43f5a620d
LelandJones <lelandj7@outlook.com>
2025-09-09 16:40:30 -0400

    edited code

--- lab3b.java
+++ lab3b.java
@@ -3,6 +3,7 @@
 // Prof. Lehman
 // Demonstrates recursive algorithm to "flood fill"
 // an ASCII graphic
+// Leland Jones
 
 class lab3b {
     public static void main(String[] args) {
@@ -27,10 +28,24 @@ class lab3b {
         // fill same space with 'b'
         floodFill(grid, 5, 3, 'b', 'g');
         display(grid);
+
+         floodFill(grid, 5, 3, 'r', 'g');
+        System.out.println("After flood fill with r\n");
+        display(grid);
+
+        // fill border area with 'i'
+        floodFill(grid, 0, 0, 'i', 'r');
+        System.out.println("After flood filling border with i\n");
+        display(grid);
+
     } // main
 
     // flood fill 2D char array
     public static void floodFill(char temp[][], int row, int col, char fillColor, char borderColor) {
+
+        if (row < 0 || row >= temp.length || col < 0 || col >= temp[0].length) {
+            return; // out of bounds
+        }
 
         // base case is when position is already filled or hit border
         if (temp[row][col] != fillColor && temp[row][col] != borderColor) {
@@ -41,7 +56,7 @@ class lab3b {
             // recursive calls to fill
             floodFill(temp, row, col - 1, fillColor, borderColor); // fill left
             floodFill(temp, row, col + 1, fillColor, borderColor); // fill right
-
+            floodFill(temp, row - 1, col, fillColor, borderColor); // fill Up
             floodFill(temp, row + 1, col, fillColor, borderColor); // fill down
 
         }
